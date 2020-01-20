16f9d12a3d636646098e457ebb7a5e3e18434635
Oded <odedlevy02@gmail.com>
2020-01-21 00:35:17 +0200

    fix bug in build query

--- generators/app/templates/apiGateway/pathResolver.js
+++ generators/app/templates/apiGateway/pathResolver.js
@@ -4,8 +4,12 @@ import { appendPropertiesToQueryParams } from "./appendPropertiesToQuery";
 export function pathResolver(targetPath, propsAppend: IRouteAppendToQueryConfig[]) {
     return (req) => {
         let baseUrl = req.url; //by default return the url as is
-        if (targetPath) { //if a target path is defined - then use it
+        if (targetPath) { //if a target path is defined - then use it after appending the query params
+            let baseWithQuery =baseUrl.split("?")
             baseUrl = targetPath;
+            if(baseWithQuery.length>1){
+                baseUrl = `${baseUrl}?${baseWithQuery[1]}`
+            }
         }
         if (propsAppend) { //if append query params - then append to the url
             baseUrl = appendPropertiesToQueryParams(baseUrl, req, propsAppend)
